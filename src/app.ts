1e4eab0d4f5e9da0f25731bd5c9fbde21d5b0d8a
ezequielbugnon <bugnonezequiel@gmail.com>
2024-01-20 16:26:39 -0300

    feat(backend): Add project struture

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+(() => {
+    main()
+})()
+
+async function main() {
+    console.log('main')
+}
+
+// Import the functions you need from the SDKs you need
+/*import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqwqcBCiGON6eQY1BKHJY7gaoxwAPiKpU",
+  authDomain: "novadata-test.firebaseapp.com",
+  projectId: "novadata-test",
+  storageBucket: "novadata-test.appspot.com",
+  messagingSenderId: "495401180135",
+  appId: "1:495401180135:web:f4cb3c8ed581330b8ffb42"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);*/
